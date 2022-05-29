ad9fef911a2de6c02e4063b4183fbb31695c37cb
Daniel Haas <daniel.c.haas@gmail.com>
2022-05-29 06:26:17 -0400

    Basic math operations added. Filled out calculator.js with functions for add, subtract, divide and multiply. Created an operate function that takes in two numbers and one of the above operations and outpus the results.

---
+++ calculator.js
@@ -0,0 +1,20 @@
+// create the operations
+function add(a,b) {
+    return a+b;
+}
+
+function subtract(a,b) {
+    return a-b;
+}
+
+function multiply(a,b) {
+    return a*b;
+}
+
+function divide(a,b) {
+    return a/b;
+}
+
+function operate(a,b,operator) {
+    return operator(a,b);
+}
